103202376eea5a6f7c927162021c3dc774da256d
Frost1801 <sergio.cibecchini@gmail.com>
2021-09-09 18:52:35 +0200

    added initial project files

---
+++ cmake-build-debug/CMakeFiles/ReworkedBankAccount.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReworkedBankAccount.dir/main.cpp.o"
+  "ReworkedBankAccount"
+  "ReworkedBankAccount.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReworkedBankAccount.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
